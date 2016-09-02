4558b497e8a27f37e62dfb7ae4e5b8be3930b451
mhy001 <mark.h.yang@gmail.com>
2016-09-01 20:12:27 -0700

    Create Login View

---
+++ OnTheMap/LoginViewController.swift
@@ -0,0 +1,13 @@
+//
+//  LoginViewController.swift
+//  OnTheMap
+//
+//  Created by Mark Yang on 9/1/16.
+//  Copyright © 2016 Myang. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+    
+}
